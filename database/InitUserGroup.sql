c0af4743d0703c69e67723fc8efe2df59dff2bdc
Jie Shen <745689122@qq.com>
2013-02-24 21:48:28 +0800

    初始化用户组表

---
+++ database/InitUserGroup.sql
@@ -0,0 +1,5 @@
+use Silicon;
+insert into ST_USER_GROUP(user_group_id, user_group_name, create_time)values('STUG0000000000000000000000000001', '普通用户', now());
+insert into ST_USER_GROUP(user_group_id, user_group_name, create_time)values('STUG0000000000000000000000000002', '会员用户', now());
+insert into ST_USER_GROUP(user_group_id, user_group_name, create_time)values('STUG0000000000000000000000000003', '系统管理员', now());
+
