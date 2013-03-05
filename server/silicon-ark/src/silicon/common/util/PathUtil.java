5468968e8e5f51a471b3e8cbb516f190bfbac496
沈杰 <sendshenjieamail@gmail.com>
2013-03-06 00:43:08 +0800

    新增：为平台配置环境变量。

---
+++ server/silicon-ark/src/silicon/common/util/PathUtil.java
@@ -0,0 +1,18 @@
+package silicon.common.util;
+
+public class PathUtil {
+	
+	public static String combineUrlPath(String...m_paths)
+	{
+		String _path = null;
+		
+		return _path;
+	}
+	
+	public static String combineFilePath(String...m_paths)
+	{
+		String _path = null;
+		
+		return _path;
+	}
+}
