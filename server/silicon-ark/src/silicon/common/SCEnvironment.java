c1c0ababc3de2fa612d880cd0de4c04fb8089e0a
沈杰 <sendshenjieamail@gmail.com>
2013-03-08 00:32:11 +0800

    Revert "新增：为平台配置环境变量。"
    This reverts commit 5468968e8e5f51a471b3e8cbb516f190bfbac496.

--- server/silicon-ark/src/silicon/common/SCEnvironment.java
+++ server/silicon-ark/src/silicon/common/SCEnvironment.java
@@ -1,9 +1,5 @@
 package silicon.common;
 
-import silicon.common.util.FileSystemUtil;
-import silicon.common.util.PathUtil;
-import java.io.File;
-
 public class SCEnvironment {
 	
 	private static String _rootPhysicalPath = null;
@@ -14,7 +10,7 @@ public class SCEnvironment {
 	
 	public static void SetRootPhysicalPath(String m_path)
 	{
-		if(FileSystemUtil.hasFile(m_path))
+		if(//是否存在)
 		{
 			_rootPhysicalPath = m_path;
 		}
@@ -33,50 +29,6 @@ public class SCEnvironment {
 	public static String GetWebContentPath()
 	{
 		return _webContentPath;
-	}
-	
-	private static String _sharePath = null;
-
-	//尚未完成函数
-	public static String GetPhysicalPath(String m_relativePath)
-	{
-		return "";
-	}
-	
-	public static String GetPhysicalFile(String m_relativePath)
-	{
-		return "";
-	}
-	
-	public static String GetSharedPath()
-	{
-		if(_sharePath == null)
-		{
-			_sharePath = System.getenv("SC_SHARED");
-			if(_sharePath == null)
-			{
-				throw new SCRuntimeException("没有在操作系统中为环境变量设定“" + _sharePath+ "”并指向共享变量");
-			}
-			
-			if(!FileSystemUtil.hasDirectory(_sharePath))
-			{
-				throw new SCRuntimeException("当前环境变量SC_SHARED所指向的目录不存在");
-			}
-		}
-		
-		return _sharePath;
-	}
-	
-	public static String GetSharedPath(String m_relativePath)
-	{
-		String _path = PathUtil.combineFilePath(_sharePath, m_relativePath);
-		return _path;
-	}
-	
-	public static File GetSharedFile(String m_relativePath)
-	{
-		String _path = GetSharedPath(m_relativePath);
-		return new File(_path);
 	}
 	
 
