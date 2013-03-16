2e782d4797760062ee8c45bfb3cca972782fce65
Jie Shen <745689122@qq.com>
2013-03-16 17:32:24 +0800

    web工程添加listener监听

--- server/Source/silicon-ark/src/silicon/ark/web/listener/AppContextListener.java
+++ server/Source/silicon-ark/src/silicon/ark/web/listener/AppContextListener.java
@@ -10,12 +10,14 @@ public class AppContextListener implements ServletContextListener
 	@Override
 	public void contextDestroyed(ServletContextEvent arg0) {
 		// TODO Auto-generated method stub
+		System.out.print("stop ok");
 		ArkService.getInstance().start();
 	}
 
 	@Override
 	public void contextInitialized(ServletContextEvent arg0) {
 		// TODO Auto-generated method stub
+		System.out.print("start ....");
 		ArkService.getInstance().stop();
 	}
 
